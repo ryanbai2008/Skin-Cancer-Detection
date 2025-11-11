bf97e67e3608840151f9f3f2a9eae25eb9f10b02
ryanbai2008 <69367510+ryanbai2008@users.noreply.github.com>
2025-11-11 15:35:37 -0800

    Comment out prediction button event listener
    Comment out the prediction button functionality and related code.

--- script.js
+++ script.js
@@ -268,55 +268,58 @@ input.addEventListener("change", e => {
   reader.readAsDataURL(file);
 });
 
+// UNCOMMENT IF WANT TO PUT PREDICTION BUTTON ON MY WEBSITE AGAIN, IF EVER CONNECTED TO BACKEND
+// UNCOMMENT IF WANT TO PUT PREDICTION BUTTON ON MY WEBSITE AGAIN, IF EVER CONNECTED TO BACKEND
+// UNCOMMENT IF WANT TO PUT PREDICTION BUTTON ON MY WEBSITE AGAIN, IF EVER CONNECTED TO BACKEND
 // Predict button
-predictBtn.addEventListener("click", async () => {
-  if (!imageData) {
-    alert("Please upload an image first!");
-    return;
-  }
+// predictBtn.addEventListener("click", async () => {
+//   if (!imageData) {
+//     alert("Please upload an image first!");
+//     return;
+//   }
 
-  result.textContent = "Running model...";
+//   result.textContent = "Running model...";
 
-  try {
-    // Load ONNX model (web-friendly)
-    const session = await ort.InferenceSession.create("assets/model.onnx", {
-      executionProviders: ['wasm']  // or 'webgl' / 'webgpu' if supported
-    });
+//   try {
+//     // Load ONNX model (web-friendly)
+//     const session = await ort.InferenceSession.create("assets/model.onnx", {
+//       executionProviders: ['wasm']  // or 'webgl' / 'webgpu' if supported
+//     });
 
-    // Preprocess image to Float32 tensor [1, 3, 224, 224]
-    const data = new Float32Array(3 * 224 * 224);
-    for (let i = 0; i < 224 * 224; i++) {
-      const offset = i * 4;
-      data[i] = imageData.data[offset] / 255.0;          // R
-      data[i + 224 * 224] = imageData.data[offset + 1] / 255.0;  // G
-      data[i + 2 * 224 * 224] = imageData.data[offset + 2] / 255.0; // B
-    }
+//     // Preprocess image to Float32 tensor [1, 3, 224, 224]
+//     const data = new Float32Array(3 * 224 * 224);
+//     for (let i = 0; i < 224 * 224; i++) {
+//       const offset = i * 4;
+//       data[i] = imageData.data[offset] / 255.0;          // R
+//       data[i + 224 * 224] = imageData.data[offset + 1] / 255.0;  // G
+//       data[i + 2 * 224 * 224] = imageData.data[offset + 2] / 255.0; // B
+//     }
 
-    const inputName = session.inputNames[0];  // automatically detect input name
-    const tensor = new ort.Tensor("float32", data, [1, 3, 224, 224]);
-    const feeds = {};
-    feeds[inputName] = tensor;
+//     const inputName = session.inputNames[0];  // automatically detect input name
+//     const tensor = new ort.Tensor("float32", data, [1, 3, 224, 224]);
+//     const feeds = {};
+//     feeds[inputName] = tensor;
 
-    // Run inference
-    const output = await session.run(feeds);
-    const outputTensor = output[session.outputNames[0]].data;
+//     // Run inference
+//     const output = await session.run(feeds);
+//     const outputTensor = output[session.outputNames[0]].data;
 
-    // Softmax
-    const max = Math.max(...outputTensor);
-    const exps = outputTensor.map(x => Math.exp(x - max));
-    const sumExps = exps.reduce((a, b) => a + b, 0);
-    const probs = exps.map(x => x / sumExps);
+//     // Softmax
+//     const max = Math.max(...outputTensor);
+//     const exps = outputTensor.map(x => Math.exp(x - max));
+//     const sumExps = exps.reduce((a, b) => a + b, 0);
+//     const probs = exps.map(x => x / sumExps);
 
-    // Get top prediction
-    const maxIdx = probs.indexOf(Math.max(...probs));
-    const label = labels[maxIdx];
-    const confidence = (probs[maxIdx] * 100).toFixed(1);
+//     // Get top prediction
+//     const maxIdx = probs.indexOf(Math.max(...probs));
+//     const label = labels[maxIdx];
+//     const confidence = (probs[maxIdx] * 100).toFixed(1);
 
-    result.innerHTML = `<b>${labelMap[label]}</b> (${label})<br>Confidence: ${confidence}%<br><br>` +
-      labels.map((l, i) => `${labelMap[l]}: ${(probs[i] * 100).toFixed(1)}%`).join("<br>");
-  } catch (err) {
-    console.error(err);
-    result.textContent = `Error running model: ${err.message}`;
-  }
-});
+//     result.innerHTML = `<b>${labelMap[label]}</b> (${label})<br>Confidence: ${confidence}%<br><br>` +
+//       labels.map((l, i) => `${labelMap[l]}: ${(probs[i] * 100).toFixed(1)}%`).join("<br>");
+//   } catch (err) {
+//     console.error(err);
+//     result.textContent = `Error running model: ${err.message}`;
+//   }
+// });
 
