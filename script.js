df829b3d475855c99b1a266f36b0edc8af320bc2
ryanbai2008 <69367510+ryanbai2008@users.noreply.github.com>
2026-02-09 17:31:49 -0800

    Update example classifications in script.js

--- script.js
+++ script.js
@@ -140,10 +140,10 @@ if (currentExample == 1) {
   exampleResult.innerHTML = `<p><strong>Example of Melanoma classification</strong></p>`;
 }
 else if (currentExample == 2) {
-  exampleResult.innerHTML = `<p><strong>Example of Melanoma classification as the most likely condition with Nevus following as a close second. This classification was accurate, but shows that there can be some uncertainty when distinguishing between two similar conditions</strong></p>`;
+  exampleResult.innerHTML = `<p><strong>Example of Benign Keratosis-Like Lesion classification</strong></p>`;
 }
 else if (currentExample == 3) {
-  exampleResult.innerHTML = `<p><strong>Example of Basal Cell Carcinoma classification</strong></p>`;
+  exampleResult.innerHTML = `<p><strong>Example of Dermatofibroma classification</strong></p>`;
 }
 
 
